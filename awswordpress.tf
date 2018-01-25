ea257be814f76d87a367699d5273de4d39a4c84a
root <root@localhost.localdomain>
2018-01-25 12:05:54 +0100

    template wordpress et variables

---
+++ awswordpress.tf
@@ -0,0 +1,18 @@
+#Configure the AWS Provider
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "us-east-1"
+}
+
+#Create a web server
+
+resource "aws_instance" "WordPress3" {
+  key_name = "KeyINSTA"
+  security_groups = [
+  "MyDMZ"
+  ]
+  ami = "ami-b9b784c3"
+  instance_type = "t2.micro"
+
+}
